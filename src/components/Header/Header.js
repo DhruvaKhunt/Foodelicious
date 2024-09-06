9443a8aa9d2f4514b246b9dea692341a7073eaee
Dhruva Khunt <dhruvapatel02102003@gmail.com>
2024-09-06 12:04:27 +0530

    first-commit

---
+++ src/components/Header/Header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className='header'>
+        <div className="header-contents">
+            <h2>Order your favourite food here</h2>
+            <p>Enjoy delicious meals delivered to your door with Foodelicious. Fast, fresh, and flavorful food from your favorite local restaurants</p>
+            <button>View Menu</button>
+        </div>
+    </div>
+  )
+}
+
+export default Header
